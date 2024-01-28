accd63090e78a0f1910ef3dddca9d52028e28abf
Sana Harighi <harighisana@gmail.com>
2024-01-28 17:43:12 +0330

    feat: Add Discriminator

--- Codes/model/discriminator.py
+++ Codes/model/discriminator.py
@@ -3,6 +3,22 @@ import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    """Implement the discriminator model discussed in the paper"""
+        """Implement the discriminator model discussed in the paper"""
+    def __init__(self, input_size=768, hidden_sizes=[768], num_labels=7, dropout_rate=0.1):
+        super(Discriminator, self).__init__()
+        self.input_dropout = nn.Dropout(p=dropout_rate)
+        layers = []
+        hidden_sizes = [input_size] + hidden_sizes
+        for i in range(len(hidden_sizes)-1):
+            layers.extend([nn.Linear(hidden_sizes[i], hidden_sizes[i+1]), nn.LeakyReLU(0.2, inplace=True), nn.Dropout(dropout_rate)])
 
-    pass
+        self.layers = nn.Sequential(*layers) #per il flatten
+        self.logit = nn.Linear(hidden_sizes[-1],num_labels+1) # +1 for the probability of this sample being fake/real.
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, input_rep):
+        input_rep = self.input_dropout(input_rep)
+        last_rep = self.layers(input_rep)
+        logits = self.logit(last_rep)
+        probs = self.softmax(logits)
+        return last_rep, logits, probs
