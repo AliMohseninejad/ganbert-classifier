e6ad829909a9a1024c66d3856a6f881415c3913b
Nil <nil.abbasi@yahoo.com>
2024-01-30 13:34:33 +0330

    fix: Fix visualization code

--- Codes/training/visualize.py
+++ Codes/training/visualize.py
@@ -1,43 +1,48 @@
 import matplotlib.pyplot as plt
 
 def plot_results(results):
-    epochs         = [result["epoch"]          for result in results]
-    train_loss     = [result["train_loss"]     for result in results]
-    val_loss       = [result["val_loss"]       for result in results]
-    train_accuracy = [result["train_accuracy"] for result in results]
-    val_accuracy   = [result["val_accuracy"]   for result in results]
-    train_f1_score = [result["train_f1_score"] for result in results]
-    val_f1_score   = [result["f1_score"]       for result in results]
+    epochs              = [result["epoch"]                                   for result in results]
+    train_loss          = [result["train_loss"].detach().cpu().numpy()       for result in results]
+    validation_loss     = [result["validation_loss"].detach().cpu().numpy()  for result in results]
+    train_accuracy      = [result["train_accuracy"]                          for result in results]
+    validation_accuracy = [result["validation_accuracy"]                     for result in results]
+    train_f1            = [result["train_f1"]                                for result in results]
+    validation_f1       = [result["validation_f1"]                           for result in results]
     #----------------------------------------------------------------------------------------------
     # Plot Loss
     plt.figure(figsize=(12, 4))
     plt.subplot(1, 3, 1)
-    plt.plot(epochs, train_loss, label='Train Loss'     , marker='o', linestyle='--', color= "#f70d1a", linewidth=2) #vivid red
-    plt.plot(epochs, val_loss,   label='Validation Loss', marker='o', linestyle='--', color= "#002147", linewidth=2) #oxford blue
+    plt.plot(epochs, train_loss     , label='Train Loss'     , marker='o', linestyle='--', color= "#f70d1a", linewidth=2) #vivid red
+    plt.plot(epochs, validation_loss, label='Validation Loss', marker='o', linestyle='--', color= "#002147", linewidth=2) #oxford blue
     plt.title('Training and Validation Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
+    plt.grid(True, linestyle='--', linewidth=0.4, color='#b0b0b0')  # Add thin dashed grid lines
+    plt.tight_layout()
     #----------------------------------------------------------------------------------------------
     # Plot Accuracy
     plt.subplot(1, 3, 2)
-    plt.plot(epochs, train_accuracy, label='Train Accuracy'     , marker='o', linestyle='-', color="#f70d1a", linewidth=2) #vivid red
-    plt.plot(epochs, val_accuracy,   label='Validation Accuracy', marker='o', linestyle='-', color="#002147", linewidth=2) #oxford blue
+    plt.plot(epochs, train_accuracy     , label='Train Accuracy'     , marker='o', linestyle='-', color="#f70d1a", linewidth=2) #vivid red
+    plt.plot(epochs, validation_accuracy, label='Validation Accuracy', marker='o', linestyle='-', color="#002147", linewidth=2) #oxford blue
     plt.title('Training and Validation Accuracy')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     plt.legend()
+    plt.grid(True, linestyle='--', linewidth=0.4, color='#b0b0b0')  # Add thin dashed grid lines
+    plt.tight_layout()
     #----------------------------------------------------------------------------------------------
     # plot F1
     plt.subplot(1, 3, 3)
-    plt.plot(epochs, train_f1_score, label='Train F1 Score'   , marker='o', linestyle='-', color="#f70d1a", linewidth=2) #vivid red
-    plt.plot(epochs, val_f1_score, label='Validation F1 Score', marker='o', linestyle='-', color="#002147", linewidth=2) #oxford blue
+    plt.plot(epochs, train_f1     , label='Train F1 Score'     , marker='o', linestyle='-', color="#f70d1a", linewidth=2) #vivid red
+    plt.plot(epochs, validation_f1, label='Validation F1 Score', marker='o', linestyle='-', color="#002147", linewidth=2) #oxford blue
     plt.title('Training and Validation F1 score')
     plt.xlabel('Epochs')
     plt.ylabel('F1 score')
     plt.legend()
-    #----------------------------------------------------------------------------------------------
     plt.grid(True, linestyle='--', linewidth=0.4, color='#b0b0b0')  # Add thin dashed grid lines
     plt.tight_layout()
+
+
     plt.show()
 
